c35db4e1dc6a32eb99e0e50c28efc204c64e2631
xinye <xinye@anve.com>
2022-07-27 11:39:57 +0800

    remove to get

--- src/state/room/users.rs
+++ src/state/room/users.rs
@@ -69,7 +69,7 @@ impl<'r> RoomUsers {
         drop(registrations);
 
         let users = self.room.users.read().await;
-        let user = users.get(&registration)?;
+        let user = users.get(registration)?;
         let id = {
             let mut user = user.write().await;
             user.register().await;
