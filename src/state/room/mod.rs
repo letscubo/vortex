a3418cafd5980f0f057b6923a2d1e89a1fb017da
xinye <xinye@anve.com>
2022-08-12 04:29:49 +0800

    println

--- src/state/room/mod.rs
+++ src/state/room/mod.rs
@@ -54,11 +54,11 @@ impl Room {
         let worker = get_worker_pool().get_worker();
 
         let mut options = RouterOptions::default();
-        // options.media_codecs.push(crate::rtc::create_opus_codec(2));
-        // options.media_codecs.push(crate::rtc::create_vp9_codec());
+        options.media_codecs.push(crate::rtc::create_opus_codec(2));
+        options.media_codecs.push(crate::rtc::create_vp9_codec());
 
-        let s = get_supported_rtp_capabilities().codecs;
-        options.media_codecs.extend_from_slice(&s);
+        // let s = get_supported_rtp_capabilities().codecs;
+        // options.media_codecs.extend_from_slice(&s);
         let router = worker
             // .create_router(RouterOptions::new(get_supported_rtp_capabilities().codecs))
             .create_router(RouterOptions::new(options.media_codecs))
