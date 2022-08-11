a577bd88163648d906c61347601bb63779534ce4
xinye <xinye@anve.com>
2022-08-12 04:38:28 +0800

    println

--- src/rtc/mod.rs
+++ src/rtc/mod.rs
@@ -18,6 +18,40 @@ use types::{
     TransportInitData, WebRtcTransportInitData,
 };
 
+pub fn media_codecs() -> Vec<RtpCodecCapability> {
+    vec![
+        RtpCodecCapability::Audio {
+            mime_type: MimeTypeAudio::Opus,
+            preferred_payload_type: None,
+            clock_rate: NonZeroU32::new(48000).unwrap(),
+            channels: NonZeroU8::new(2).unwrap(),
+            parameters: RtpCodecParametersParameters::from([
+                ("useinbandfec", 1_u32.into()),
+                ("foo", "bar".into()),
+            ]),
+            rtcp_feedback: vec![],
+        },
+        RtpCodecCapability::Video {
+            mime_type: MimeTypeVideo::Vp8,
+            preferred_payload_type: None,
+            clock_rate: NonZeroU32::new(90000).unwrap(),
+            parameters: RtpCodecParametersParameters::default(),
+            rtcp_feedback: vec![],
+        },
+        RtpCodecCapability::Video {
+            mime_type: MimeTypeVideo::H264,
+            preferred_payload_type: None,
+            clock_rate: NonZeroU32::new(90000).unwrap(),
+            parameters: RtpCodecParametersParameters::from([
+                ("level-asymmetry-allowed", 1_u32.into()),
+                ("packetization-mode", 1_u32.into()),
+                ("profile-level-id", "4d0032".into()),
+            ]),
+            rtcp_feedback: vec![], // Will be ignored.
+        },
+    ]
+}
+
 pub fn create_opus_codec(channels: u8) -> RtpCodecCapability {
     RtpCodecCapability::Audio {
         mime_type: MimeTypeAudio::Opus,
--- src/state/room/mod.rs
+++ src/state/room/mod.rs
@@ -52,16 +52,16 @@ impl Room {
         }
 
         let worker = get_worker_pool().get_worker();
-
-        let mut options = RouterOptions::default();
-        options.media_codecs.push(crate::rtc::create_opus_codec(2));
-        options.media_codecs.push(crate::rtc::create_vp9_codec());
+        // let mut options = RouterOptions::default();
+        // options.media_codecs.push(crate::rtc::create_opus_codec(2));
+        // options.media_codecs.push(crate::rtc::create_vp9_codec());
 
         // let s = get_supported_rtp_capabilities().codecs;
         // options.media_codecs.extend_from_slice(&s);
         let router = worker
             // .create_router(RouterOptions::new(get_supported_rtp_capabilities().codecs))
-            .create_router(RouterOptions::new(options.media_codecs))
+            // .create_router(RouterOptions::new(options.media_codecs))
+            .create_route(RouterOptions::new(crate::rtc::media_codecs()))
             .await
             .map_err(|_| ApiError::InternalServerError)?;
 
